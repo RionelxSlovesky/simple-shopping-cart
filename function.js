51871ba645dbe26890caba0089f9dadccc86b075
RionelxSlovesky <misteradnan1@gmail.com>
2023-02-16 01:21:47 +0600

    Summary of total cost done
    Calculated and displayed total cost, tax and total after tax, and its a wrap for this project

--- function.js
+++ function.js
@@ -1,30 +1,56 @@
 const btnCaseField = document.getElementById('btn-case-field');
 const btnCasePlus = document.getElementById('btn-case-plus');
 const btnCaseMinus = document.getElementById('btn-case-minus');
-const caseTotal = document.getElementById('case-total');
 
 const btnIphoneField = document.getElementById('btn-iphone-field');
 const btnIphonePlus = document.getElementById('btn-iphone-plus');
 const btnIphoneMinus = document.getElementById('btn-iphone-minus');
 
+const itemTotal = document.getElementsByClassName('item-total');
+const totalCost = document.getElementById('total-cost');
+const tax = document.getElementById('tax');
+const finalCost = document.getElementById('final-cost');
 
-console.log(btnCaseField.parentNode.parentNode.childNodes[3].childNodes[1].innerText)
+function updateSummary(){
+
+    let total = 0;
+
+    for(i of itemTotal){
+        total = total + parseFloat(i.innerText);
+    }
+
+    totalCost.innerText = total;
+
+    const taxVal = ((total*15)/100).toFixed(1);
+
+    tax.innerText = taxVal;
+
+    const final = parseFloat(total)  + parseFloat(taxVal);
+
+    finalCost.innerText = final;
+
+}
+
+updateSummary();
+
 
 function updateItemCost(isIncrement, field){
 
-    const oldCost = parseInt(field.parentNode.parentNode.childNodes[3].childNodes[1].innerText);
+    const oldCost = parseFloat(field.parentNode.parentNode.childNodes[3].childNodes[1].innerText);
 
     let singleCost;
 
     if(isIncrement==true){
-        singleCost = oldCost / (parseInt(field.value)-1);
+        singleCost = oldCost / (parseFloat(field.value)-1);
     }else{
-        singleCost = oldCost / (parseInt(field.value)+1);
+        singleCost = oldCost / (parseFloat(field.value)+1);
     }
 
-    const newCost = singleCost * parseInt(field.value);
+    const newCost = singleCost * parseFloat(field.value);
 
     field.parentNode.parentNode.childNodes[3].childNodes[1].innerText = newCost;
+
+    updateSummary();
 
 }
 
